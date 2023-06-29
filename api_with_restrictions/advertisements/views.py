499f27811318d1d2b1f4e67191a5fc9fda8a6ec3
Baranova Ekaterina <recrane@rambler.ru>
2023-06-29 21:17:02 +1000

    add file

---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,27 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+from django_filters.rest_framework import DjangoFilterBackend
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    filter_backends = [filters.DjangoFilterBackend]
+    filter_set_class = AdvertisementFilter
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+        return []
